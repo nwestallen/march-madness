4208744dd5f342b77fdf5933a0dbdbeb27462e58
Nick <nwestallen@gmail.com>
2023-03-14 13:21:37 -0400

    initial setup, adds brackets dep

---
+++ src/components/MarchBracket.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Bracket, IRoundProps } from 'react-brackets';
+
+const rounds: IRoundProps[] = [
+  {
+    title: 'Round one',
+    seeds: [
+      {
+        id: 1,
+        date: new Date().toDateString(),
+        teams: [{ name: 'Team A' }, { name: 'Team B' }],
+      },
+      {
+        id: 2,
+        date: new Date().toDateString(),
+        teams: [{ name: 'Team C' }, { name: 'Team D' }],
+      },
+    ],
+  },
+  {
+    title: 'Round two',
+    seeds: [
+      {
+        id: 3,
+        date: new Date().toDateString(),
+        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+      },
+    ],
+  },
+];
+
+type Props = {}
+
+const MarchBracket = (props: Props) => {
+  return (
+    <div>
+        <Bracket rounds={rounds} />
+    </div>
+  )
+}
+
+export default MarchBracket
