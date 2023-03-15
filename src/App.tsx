ffeca7e8330d62b46e51e755b719f509ddc7453f
Nick <nwestallen@gmail.com>
2023-03-14 22:10:10 -0400

    adds mock data, bracket manipulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import './index.css';
 import RegionBracket from './components/RegionBracket';
+import { IRenderSeedProps } from 'react-brackets';
+import CustomSeed from './components/CustomSeed';
+
+const four: IRenderSeedProps = {
+  seed: {id: 1, teams: []},
+  roundIndex: 1,
+  seedIndex: 1,
+  breakpoint: 100
+}
 
 function App() {
   return (
@@ -9,6 +18,9 @@ function App() {
       <div className='flex'>
       <RegionBracket/>
       <RegionBracket rtl/>
+      </div>
+      <div className='w-1/6'>
+        <CustomSeed seed={four.seed} roundIndex={four.roundIndex} seedIndex={four.seedIndex} breakpoint={four.breakpoint}/>
       </div>
       <div className='flex'>
       <RegionBracket/>
--- src/components/RegionBracket.tsx
+++ src/components/RegionBracket.tsx
@@ -1,89 +1,130 @@
 import React, { useState } from 'react'
 import { Bracket, IRoundProps } from 'react-brackets';
+import { mockTeams } from '../constants/mockData';
 import CustomSeed from './CustomSeed'
 
 const roundsInit: IRoundProps[] = [
   {
-    title: '',
+    title: 'Round 1',
     seeds: [
       {
         id: 1,
-        teams: [{ name: 'Team A' }, { name: 'Team B' }],
+        teams: [{ name: 'slot 0' }, { name: 'slot 2' }],
       },
       {
         id: 2,
-        teams: [{ name: 'Team C' }, { name: 'Team D' }],
+        teams: [{ name: 'slot 4' }, { name: 'slot 6' }],
       },
       {
         id: 3,
-        teams: [{ name: '' }, { name: '' }],
+        teams: [{ name: 'slot 8' }, { name: 'slot 10' }],
       },
       {
         id: 4,
-        teams: [{ name: '' }, { name: '' }],
+        teams: [{ name: 'slot 12' }, { name: 'slot 16' }],
       },
       {
         id: 5,
-        teams: [{ name: '' }, { name: '' }],
+        teams: [{ name: 'slot 18' }, { name: 'slot 20' }],
       },
       {
         id: 6,
-        teams: [{ name: '' }, { name: '' }],
+        teams: [{ name: 'slot 22' }, { name: 'slot 24' }],
       },
       {
         id: 7,
-        teams: [{ name: '' }, { name: '' }],
+        teams: [{ name: 'slot 26' }, { name: 'slot 28' }],
       },
       {
         id: 8,
+        teams: [{ name: 'slot 30' }, { name: 'slot 32' }],
+      },
+    ],
+  },
+  {
+    title: 'Round 2',
+    seeds: [
+      {
+        id: 1,
+        teams: [{ name: '' }, { name: '' }],
+      },
+      {
+        id: 2,
+        teams: [{ name: '' }, { name: '' }],
+      },
+      {
+        id: 3,
+        teams: [{ name: '' }, { name: '' }],
+      },
+      {
+        id: 4,
         teams: [{ name: '' }, { name: '' }],
       },
     ],
   },
   {
-    title: '',
+    title: 'Sweet 16',
     seeds: [
       {
         id: 1,
-        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+        teams: [{ name: '' }, { name: '' }],
       },
       {
         id: 2,
-        teams: [{ name: 'Team A' }, { name: 'Team C' }],
-      },
-      {
-        id: 3,
-        teams: [{ name: 'Team A' }, { name: 'Team C' }],
-      },
-      {
-        id: 4,
-        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+        teams: [{ name: '' }, { name: '' }],
       },
     ],
   },
   {
-    title: '',
+    title: 'Elite 8',
     seeds: [
       {
         id: 1,
-        teams: [{ name: 'Team A' }, { name: 'Team C' }],
-      },
-      {
-        id: 2,
-        teams: [{ name: 'Team A' }, { name: 'Team C' }],
-      },
-    ],
-  },
+        teams: [{name: ''}, {name: ''}]
+      }
+    ]
+  }
 ];
 
-type Props = { rtl?: boolean }
+type RegionBracketProps = { 
+  rtl?: boolean
+  r1Teams?: any[]
+}
 
-const RegionBracket = ({ rtl }: Props) => {
+const sampleTeams = mockTeams.slice(0,8)
+
+const mapRound = (roundTeams: any[]) => {
+  let teamPairs = roundTeams.map(team => ({name: team.team_name})).reduce(
+    (result: any[], val, ind, array: any[]) => {
+      if(ind % 2 === 0)
+      result.push(array.slice(ind, ind + 2));
+      return result;
+    }, [])
+  return {
+    title: 'Elite 8',
+    seeds: teamPairs.map((pair, ind) => ({id: ind, teams: pair}))
+  }
+}
+
+const RegionBracket = ({ rtl, r1Teams }: RegionBracketProps) => {
 
     const [ rounds, setRounds ] = useState<IRoundProps[]>(roundsInit)
+
+    const handleCalc = () => {
+      const roundOne: IRoundProps = {
+        title: 'Elite 8',
+        seeds: [{ id: 1, teams: [ {name: 'test'}, {name: 'success'}]}]
+      }
+      setRounds(
+        [...rounds.filter(round => round.title !== 'Elite 8'), roundOne]
+      )
+      console.log(mapRound(sampleTeams))
+    }
+
   return (
     <div className='flex-inline border-4 text-sm w-1/2 pb-4'>
         <Bracket rounds={rounds} rtl={rtl} renderSeedComponent={CustomSeed}/>
+        <button onClick={handleCalc}>Calc</button>
     </div>
   )
 }
